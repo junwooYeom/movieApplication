10fd48f150ae3bcef7641b6a4602ff5149c8d283
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-21 22:32:10 +0900

    [FEAT] 도메인, 데이터 모듈 추가

---
+++ data/src/main/java/com/junwooyeom/data/RepositoryImpl.kt
@@ -0,0 +1,6 @@
+package com.junwooyeom.data
+
+import com.junwooyeom.domain.Repository
+
+class RepositoryImpl : Repository {
+}
---
+++ domain/src/main/java/com/junwooyeom/domain/Repository.kt
@@ -0,0 +1,4 @@
+package com.junwooyeom.domain
+
+interface Repository {
+}
