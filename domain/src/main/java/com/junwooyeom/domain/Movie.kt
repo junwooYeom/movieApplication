e6ff275f71e3eb0bb18f9e5b8b071072b2a6ac29
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-21 23:05:16 +0900

    [FEAT] 영화 모델 추가.

---
+++ domain/src/main/java/com/junwooyeom/domain/Movie.kt
@@ -0,0 +1,11 @@
+package com.junwooyeom.domain
+
+data class Movie(
+    val title: String,
+    val link: String,
+    val subtitle: String,
+    val pubDate: String,
+    val director: String,
+    val actor: String,
+    val userRating: Float
+)
