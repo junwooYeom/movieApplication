c811cf5796cf423e1c4af9459bbeea1b7ecdd61f
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-21 23:45:57 +0900

    [FEAT] InfraService 파라미터 추가.

--- network/src/main/java/com/junwooyeom/network/MovieInfraService.kt
+++ network/src/main/java/com/junwooyeom/network/MovieInfraService.kt
@@ -1,8 +1,12 @@
 package com.junwooyeom.network
 
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MovieInfraService {
     @GET("/v1/search/movie.json")
-    suspend fun getMoviesByQuery(): MovieResponse
+    suspend fun getMoviesByQuery(
+        @Query("query") query: String,
+        @Query("start") start: Int,
+    ): MovieResponse
 }
