9584259b06348d3f961f0e2c6607d89fa1b2c392
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-21 23:30:27 +0900

    [FEAT] 영화 네트워크 DTO 추가.

---
+++ network/src/main/java/com/junwooyeom/network/MovieDto.kt
@@ -0,0 +1,20 @@
+package com.junwooyeom.network
+
+import com.google.gson.annotations.SerializedName
+
+data class MovieDto(
+    @SerializedName("title")
+    val title: String,
+    @SerializedName("link")
+    val link: String,
+    @SerializedName("subtitle")
+    val subtitle: String,
+    @SerializedName("pubDate")
+    val pubDate: String,
+    @SerializedName("director")
+    val director: String,
+    @SerializedName("actor")
+    val actor: String,
+    @SerializedName("userRating")
+    val userRating: Float
+)
