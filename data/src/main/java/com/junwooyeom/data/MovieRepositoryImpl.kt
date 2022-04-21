d10f5c7be3934b09bcc3bc8d8f46c5a58bebaad3
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-21 23:49:58 +0900

    [FEAT] 페이징 라이브러리 추가 및 모듈 연동.

--- data/src/main/java/com/junwooyeom/data/MovieRepositoryImpl.kt
+++ data/src/main/java/com/junwooyeom/data/MovieRepositoryImpl.kt
@@ -1,6 +1,12 @@
 package com.junwooyeom.data
 
+import androidx.paging.PagingData
+import com.junwooyeom.domain.Movie
 import com.junwooyeom.domain.MovieRepository
+import kotlinx.coroutines.flow.Flow
 
 class MovieRepositoryImpl : MovieRepository {
+    override fun getMovies(): Flow<PagingData<Movie>> {
+        TODO("Not yet implemented")
+    }
 }
--- domain/src/main/java/com/junwooyeom/domain/MovieRepository.kt
+++ domain/src/main/java/com/junwooyeom/domain/MovieRepository.kt
@@ -1,4 +1,9 @@
 package com.junwooyeom.domain
 
+import androidx.paging.PagingData
+import kotlinx.coroutines.flow.Flow
+
 interface MovieRepository {
+
+    fun getMovies(): Flow<PagingData<Movie>>
 }
