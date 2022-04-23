f997c5492ba3f3f2acaf383787a83b880374cbda
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-23 21:53:59 +0900

    [FEAT] 검색 UI 설정 및 MovieFragment 설정.

--- app/src/main/java/com/junwooyeom/movieapplication/MovieFragment.kt
+++ app/src/main/java/com/junwooyeom/movieapplication/MovieFragment.kt
@@ -1,8 +1,47 @@
 package com.junwooyeom.movieapplication
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
+import com.junwooyeom.movieapplication.databinding.FragmentMovieBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class MovieFragment : Fragment() {
+
+    private lateinit var binding: FragmentMovieBinding
+
+    private val viewModel by viewModels<MovieViewModel>()
+
+    private val adapter by lazy {
+        MovieAdapter()
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_movie, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentMovieBinding.bind(view)
+    }
+
+    fun getMovie(query: String) {
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewModel.getMovie(query).collect {
+                adapter.submitData(it)
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/junwooyeom/movieapplication/MovieViewModel.kt
@@ -0,0 +1,23 @@
+package com.junwooyeom.movieapplication
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.junwooyeom.domain.GetMoviesUseCase
+import com.junwooyeom.domain.Movie
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+@HiltViewModel
+class MovieViewModel @Inject constructor(
+    private val getMoviesUseCase: GetMoviesUseCase
+) : ViewModel() {
+
+
+    fun getMovie(query: String) : Flow<PagingData<Movie>> {
+        return getMovie(query).cachedIn(viewModelScope)
+    }
+
+}
