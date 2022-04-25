40d43234d494738c3eab58d8424ca7625f2c1184
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-25 22:36:52 +0900

    [FEAT] NavController 설정.

--- app/src/main/java/com/junwooyeom/movieapplication/MainActivity.kt
+++ app/src/main/java/com/junwooyeom/movieapplication/MainActivity.kt
@@ -2,11 +2,20 @@ package com.junwooyeom.movieapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.junwooyeom.movieapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     private lateinit var binding: ActivityMainBinding
 
@@ -14,7 +23,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
-
         setContentView(binding.root)
+
+        navController = binding.navHostFragmentContainer.findNavController()
+
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.movieFragment)
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration)
     }
 }
