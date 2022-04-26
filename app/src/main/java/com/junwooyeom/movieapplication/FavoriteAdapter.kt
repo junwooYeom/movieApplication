d96da6a17ac056170b5d11e825df1b415cdf9738
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-26 20:48:24 +0900

    [FEAT] 즐겨찾기 어댑터 추가.

---
+++ app/src/main/java/com/junwooyeom/movieapplication/FavoriteAdapter.kt
@@ -0,0 +1,57 @@
+package com.junwooyeom.movieapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.junwooyeom.domain.Movie
+import com.junwooyeom.movieapplication.databinding.ItemMovieBinding
+
+class FavoriteAdapter(
+    private val isMovieSelected: (Movie) -> Unit,
+    private val isMovieFavoriteSelected: (Movie, Boolean) -> Unit
+) : ListAdapter<Movie, FavoriteAdapter.ViewHolder>(movieComparator){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemMovieBinding.inflate(LayoutInflater.from(parent.context), parent, false),
+            isMovieSelected,
+            isMovieFavoriteSelected
+        )
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(getItem(position), true)
+    }
+
+
+    class ViewHolder(
+        private val binding: ItemMovieBinding,
+        private val isMovieSelected: (Movie) -> Unit,
+        private val isMovieFavoriteSelected: (Movie, Boolean) -> Unit
+    ) : RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: Movie, isSelected: Boolean) {
+            binding.movie = item
+            binding.isSelected = isSelected
+            binding.root.setOnClickListener {
+                isMovieSelected(item)
+            }
+            binding.btnFavorite.setOnClickListener {
+                isMovieFavoriteSelected(item, isSelected)
+            }
+        }
+    }
+
+    private companion object {
+        val movieComparator = object : DiffUtil.ItemCallback<Movie>() {
+            override fun areItemsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem.title == newItem.title
+            }
+
+            override fun areContentsTheSame(oldItem: Movie, newItem: Movie): Boolean {
+                return oldItem == newItem
+            }
+
+        }
+    }
+}
