8b10af25230561e75e8320ada1fca4066d5c42c2
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-27 17:44:16 +0900

    [FEAT] 액티비티 NavController 설정.

--- app/src/main/java/com/junwooyeom/movieapplication/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/junwooyeom/movieapplication/presentation/activity/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
+import com.junwooyeom.movieapplication.R
 import com.junwooyeom.movieapplication.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -16,10 +17,19 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var appBarConfiguration : AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment_container)
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.movieFragment)
+            .build()
+
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
