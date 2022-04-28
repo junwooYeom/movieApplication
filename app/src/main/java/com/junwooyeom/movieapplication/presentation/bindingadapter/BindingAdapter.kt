f1db19dcdbb437d6d3e4e60cdec3a4e04f18fe05
yl_luminate <83196369+junwooYeom@users.noreply.github.com>
2022-04-28 16:10:25 +0900

    [FEAT] 데이터베이스 UI 추가 및 Glide 그리기 오류 추가.

--- app/src/main/java/com/junwooyeom/movieapplication/presentation/bindingadapter/BindingAdapter.kt
+++ app/src/main/java/com/junwooyeom/movieapplication/presentation/bindingadapter/BindingAdapter.kt
@@ -3,11 +3,16 @@ package com.junwooyeom.movieapplication.presentation.bindingadapter
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import com.junwooyeom.movieapplication.GlideApp
+import com.junwooyeom.movieapplication.R
 
 object BindingAdapter {
     @BindingAdapter("imageUrl")
     @JvmStatic
     fun load(imageView: ImageView, url: String) {
-        GlideApp.with(imageView.context).load(url).into(imageView)
+        if (url.isEmpty()) {
+            imageView.setBackgroundResource(R.drawable.ic_baseline_image_not_supported_24)
+        } else {
+            GlideApp.with(imageView.context).load(url).into(imageView)
+        }
     }
 }
--- app/src/main/java/com/junwooyeom/movieapplication/presentation/viewmodel/FavoriteViewModel.kt
+++ app/src/main/java/com/junwooyeom/movieapplication/presentation/viewmodel/FavoriteViewModel.kt
@@ -1,20 +1,34 @@
 package com.junwooyeom.movieapplication.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.junwooyeom.domain.usecase.GetFavoritesUseCase
 import com.junwooyeom.domain.model.Movie
+import com.junwooyeom.domain.usecase.AddToFavoriteUseCase
+import com.junwooyeom.domain.usecase.DeleteToFavoriteUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class FavoriteViewModel @Inject constructor(
-    private val getFavoritesUseCase: GetFavoritesUseCase
+    private val getFavoritesUseCase: GetFavoritesUseCase,
+    private val addToFavoriteUseCase: AddToFavoriteUseCase,
+    private val deleteToFavoriteUseCase: DeleteToFavoriteUseCase
 ) : ViewModel() {
 
     fun getFavorites(): Flow<List<Movie>> = getFavoritesUseCase()
 
-    fun addToFavorite(movie: Movie) {}
+    fun addToFavorite(movie: Movie) {
+        viewModelScope.launch {
+            addToFavoriteUseCase(movie)
+        }
+    }
 
-    fun deleteToFavorite(movie: Movie) {}
+    fun deleteToFavorite(movie: Movie) {
+        viewModelScope.launch {
+            deleteToFavoriteUseCase(movie)
+        }
+    }
 }
